4e80be1dab5ef2387d5873241f2da2ebce789cef
andrea <andrea@andrea-HP-Pavilion-g6-Notebook-PC>
2013-02-28 14:09:47 +0100

    commit dopo connessione al database (non so se sono escusi i file di netbeans)

--- app.js
+++ app.js
@@ -4,17 +4,18 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+, routes = require('./routes')
+, user = require('./routes/user')
+, http = require('http')
+, path = require('path');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'hbs');
+  app.set('view engine', 'html');
+  app.engine('html', require('hbs').__express);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -36,22 +37,18 @@ http.createServer(app).listen(app.get('port'), function(){
 
 
 var mysql      = require('mysql');
+var model = require("./node_modules/application/model.js");
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'test',
+  database : 'sistemidistribuiti',
   insecureAuth:true
 });
 
 
-
 connection.connect();
 
-connection.query('SELECT * FROM ciao', function(err, rows, fields) {
-  if (err) throw err;
 
-  console.log('The solution is: ', rows[0].campo);
-});
 
 connection.end();
