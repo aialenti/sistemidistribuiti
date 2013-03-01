e44ba1667ba68bc995d9c10c894974d89fabea6e
andrea <andrea@andrea-HP-Pavilion-g6-Notebook-PC>
2013-03-01 10:43:58 +0100

    commit dopo connessione al database (non so se sono escusi i file di netbeans)

--- sql/sistemidistribuiti.sql
+++ sql/sistemidistribuiti.sql
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET nameS utf8 */;
 
 --
 -- Database: `sistemidistribuiti`
@@ -27,8 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `countries` (
-  `country_id` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`country_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -36,256 +36,253 @@ CREATE TABLE IF NOT EXISTS `countries` (
 -- Dumping data for table `countries`
 --
 
-INSERT INTO `countries` (`country_id`, `name`) VALUES
-(1, 0),
-(2, 0),
-(3, 0),
-(4, 0),
-(5, 0),
-(6, 0),
-(7, 0),
-(8, 0),
-(9, 0),
-(10, 0),
-(11, 0),
-(12, 0),
-(13, 0),
-(14, 0),
-(15, 0),
-(16, 0),
-(17, 0),
-(18, 0),
-(19, 0),
-(20, 0),
-(21, 0),
-(22, 0),
-(23, 0),
-(24, 0),
-(25, 0),
-(26, 0),
-(27, 0),
-(28, 0),
-(29, 0),
-(30, 0),
-(31, 0),
-(32, 0),
-(33, 0),
-(34, 0),
-(35, 0),
-(36, 0),
-(37, 0),
-(38, 0),
-(39, 0),
-(40, 0),
-(41, 0),
-(42, 0),
-(43, 0),
-(44, 0),
-(45, 0),
-(46, 0),
-(47, 0),
-(48, 0),
-(49, 0),
-(50, 0),
-(51, 0),
-(52, 0),
-(53, 0),
-(54, 0),
-(55, 0),
-(56, 0),
-(57, 0),
-(58, 0),
-(60, 0),
-(61, 0),
-(62, 0),
-(63, 0),
-(64, 0),
-(65, 0),
-(66, 0),
-(67, 0),
-(68, 0),
-(69, 0),
-(70, 0),
-(71, 0),
-(72, 0),
-(73, 0),
-(74, 0),
-(75, 0),
-(76, 0),
-(77, 0),
-(78, 0),
-(79, 0),
-(80, 0),
-(81, 0),
-(82, 0),
-(83, 0),
-(85, 0),
-(86, 0),
-(87, 0),
-(88, 0),
-(89, 0),
-(91, 0),
-(92, 0),
-(93, 0),
-(94, 0),
-(95, 0),
-(96, 0),
-(97, 0),
-(98, 0),
-(99, 0),
-(100, 0),
-(101, 0),
-(102, 0),
-(103, 0),
-(104, 0),
-(105, 0),
-(106, 0),
-(107, 0),
-(108, 0),
-(109, 0),
-(110, 0),
-(111, 0),
-(112, 0),
-(113, 0),
-(114, 0),
-(115, 0),
-(116, 0),
-(117, 0),
-(118, 0),
-(119, 0),
-(120, 0),
-(121, 0),
-(122, 0),
-(123, 0),
-(124, 0),
-(125, 0),
-(126, 0),
-(127, 0),
-(128, 0),
-(129, 0),
-(130, 0),
-(131, 0),
-(132, 0),
-(133, 0),
-(134, 0),
-(135, 0),
-(136, 0),
-(137, 0),
-(138, 0),
-(139, 0),
-(140, 0),
-(141, 0),
-(142, 0),
-(143, 0),
-(144, 0),
-(145, 0),
-(146, 0),
-(147, 0),
-(148, 0),
-(149, 0),
-(150, 0),
-(151, 0),
-(152, 0),
-(153, 0),
-(154, 0),
-(155, 0),
-(156, 0),
-(157, 0),
-(158, 0),
-(159, 0),
-(160, 0),
-(161, 0),
-(162, 0),
-(163, 0),
-(164, 0),
-(165, 0),
-(166, 0),
-(167, 0),
-(168, 0),
-(169, 0),
-(170, 0),
-(171, 0),
-(172, 0),
-(173, 0),
-(174, 0),
-(175, 0),
-(176, 0),
-(177, 0),
-(178, 0),
-(179, 0),
-(180, 0),
-(181, 0),
-(182, 0),
-(183, 0),
-(184, 0),
-(185, 0),
-(186, 0),
-(187, 0),
-(188, 0),
-(189, 0),
-(190, 0),
-(191, 0),
-(192, 0),
-(193, 0),
-(194, 0),
-(195, 0),
-(196, 0),
-(197, 0),
-(198, 0),
-(199, 0),
-(200, 0),
-(201, 0),
-(202, 0),
-(203, 0),
-(204, 0),
-(205, 0),
-(206, 0),
-(207, 0),
-(208, 0),
-(209, 0),
-(210, 0),
-(211, 0),
-(212, 0),
-(213, 0),
-(214, 0),
-(215, 0),
-(216, 0),
-(217, 0),
-(218, 0),
-(219, 0),
-(220, 0),
-(221, 0),
-(222, 0),
-(223, 0),
-(224, 0),
-(225, 0),
-(226, 0),
-(227, 0),
-(228, 0),
-(229, 0),
-(230, 0),
-(231, 0),
-(232, 0),
-(233, 0),
-(234, 0),
-(235, 0),
-(236, 0),
-(237, 0),
-(238, 0),
-(239, 0),
-(240, 0),
-(241, 0),
-(242, 0),
-(243, 0),
-(244, 0),
-(245, 0),
-(246, 0),
-(247, 0),
-(248, 0),
-(249, 0),
-(250, 0),
-(251, 0),
-(252, 0);
+INSERT INTO countries(name) VALUES(‘Afghanistan’);
+INSERT INTO countries(name) VALUES(‘Albania’);
+INSERT INTO countries(name) VALUES(‘Algeria’);
+INSERT INTO countries(name) VALUES(‘American Samoa’);
+INSERT INTO countries(name) VALUES(‘Andorra’);
+INSERT INTO countries(name) VALUES(‘Angola’);
+INSERT INTO countries(name) VALUES(‘Anguilla’);
+INSERT INTO countries(name) VALUES(‘Antarctica’);
+INSERT INTO countries(name) VALUES(‘Antigua and Barbuda’);
+INSERT INTO countries(name) VALUES(‘Argentina’);
+INSERT INTO countries(name) VALUES(‘Armenia’);
+INSERT INTO countries(name) VALUES(‘Aruba’);
+INSERT INTO countries(name) VALUES(‘ustralia’);
+INSERT INTO countries(name) VALUES(‘Austria’);
+INSERT INTO countries(name) VALUES(‘Azerbaijan’);
+INSERT INTO countries(name) VALUES(‘Bahamas’);
+INSERT INTO countries(name) VALUES(‘Bahrain’);
+INSERT INTO countries(name) VALUES(‘Bangladesh’);
+INSERT INTO countries(name) VALUES(‘Barbados’);
+INSERT INTO countries(name) VALUES(‘Belarus ‘);
+INSERT INTO countries(name) VALUES(‘Belgium ‘);
+INSERT INTO countries(name) VALUES(‘Belize’);
+INSERT INTO countries(name) VALUES(‘Benin’);
+INSERT INTO countries(name) VALUES(‘Bermuda’);
+INSERT INTO countries(name) VALUES(‘Bhutan’);
+INSERT INTO countries(name) VALUES(‘Bolivia’);
+INSERT INTO countries(name) VALUES(‘Bosnia and Herzegowina’);
+INSERT INTO countries(name) VALUES(‘Botswana’);
+INSERT INTO countries(name) VALUES(‘Bouvet Island’);
+INSERT INTO countries(name) VALUES(‘Brazil’);
+INSERT INTO countries(name) VALUES(‘British Indian Ocean’);
+INSERT INTO countries(name) VALUES(‘Brunei Darussalam’);
+INSERT INTO countries(name) VALUES(‘Bulgaria’);
+INSERT INTO countries(name) VALUES(‘Burkina Faso ‘);
+INSERT INTO countries(name) VALUES(‘Burundi’);
+INSERT INTO countries(name) VALUES(‘Cambodia ‘);
+INSERT INTO countries(name) VALUES(‘Cameroon ‘);
+INSERT INTO countries(name) VALUES(‘anada’);
+INSERT INTO countries(name) VALUES(‘Cape Verde’);
+INSERT INTO countries(name) VALUES(‘Cayman Islands’);
+INSERT INTO countries(name) VALUES(‘Central African Republic’);
+INSERT INTO countries(name) VALUES(‘Chad’);
+INSERT INTO countries(name) VALUES(‘Chile’);
+INSERT INTO countries(name) VALUES(‘China’);
+INSERT INTO countries(name) VALUES(‘Christmas Island’);
+INSERT INTO countries(name) VALUES(‘Cocos (Keeling) Islands’);
+INSERT INTO countries(name) VALUES(‘Colombia’);
+INSERT INTO countries(name) VALUES(‘Comoros’);
+INSERT INTO countries(name) VALUES(‘Congo (Democratic Republic of the)’);
+INSERT INTO countries(name) VALUES(‘Congo (Republic of the)’);
+INSERT INTO countries(name) VALUES(‘Cook islands’);
+INSERT INTO countries(name) VALUES(‘Costa Rica’);
+INSERT INTO countries(name) VALUES(‘Cote dIvoire (Ivory Coast)’);
+INSERT INTO countries(name) VALUES(‘Croatia’);
+INSERT INTO countries(name) VALUES(‘Cuba’);
+INSERT INTO countries(name) VALUES(‘Curaco’);
+INSERT INTO countries(name) VALUES(‘Cyprus’);
+INSERT INTO countries(name) VALUES(‘Cyprus (Turkish Republic of Northern)’);
+INSERT INTO countries(name) VALUES(‘Czech Republic’);
+INSERT INTO countries(name) VALUES(‘Denmark’);
+INSERT INTO countries(name) VALUES(‘Djibouti ‘);
+INSERT INTO countries(name) VALUES(‘Dominica’);
+INSERT INTO countries(name) VALUES(‘Dominican Republic’);
+INSERT INTO countries(name) VALUES(‘East Timor’);
+INSERT INTO countries(name) VALUES(‘Ecuador’);
+INSERT INTO countries(name) VALUES(‘Egypt’);
+INSERT INTO countries(name) VALUES(‘El Salvador’);
+INSERT INTO countries(name) VALUES(‘Equatorial Guinea’);
+INSERT INTO countries(name) VALUES(‘Eritrea’);
+INSERT INTO countries(name) VALUES(‘Estonia’);
+INSERT INTO countries(name) VALUES(‘Ethiopia’);
+INSERT INTO countries(name) VALUES(‘Falkland Islands’);
+INSERT INTO countries(name) VALUES(‘Faroe Islands’);
+INSERT INTO countries(name) VALUES(‘Fiji’);
+INSERT INTO countries(name) VALUES(‘Finland’);
+INSERT INTO countries(name) VALUES(‘France’);
+INSERT INTO countries(name) VALUES(‘France, Metropolitan’);
+INSERT INTO countries(name) VALUES(‘French Guiana ‘);
+INSERT INTO countries(name) VALUES(‘French Polynesia’);
+INSERT INTO countries(name) VALUES(‘French Southern Territories’);
+INSERT INTO countries(name) VALUES(‘Gabon’);
+INSERT INTO countries(name) VALUES(‘Gambia’);
+INSERT INTO countries(name) VALUES(‘Georgia’);
+INSERT INTO countries(name) VALUES(‘Germany ‘);
+INSERT INTO countries(name) VALUES(‘Ghana’);
+INSERT INTO countries(name) VALUES(‘Gibraltar’);
+INSERT INTO countries(name) VALUES(‘Greece’);
+INSERT INTO countries(name) VALUES(‘Greenland’);
+INSERT INTO countries(name) VALUES(‘Grenada’);
+INSERT INTO countries(name) VALUES(‘Guadeloupe’);
+INSERT INTO countries(name) VALUES(‘Guam’);
+INSERT INTO countries(name) VALUES(‘Guatemala’);
+INSERT INTO countries(name) VALUES(‘Guinea’);
+INSERT INTO countries(name) VALUES(‘Guinea-Bissau’);
+INSERT INTO countries(name) VALUES(‘Guyana’);
+INSERT INTO countries(name) VALUES(‘Haiti’);
+INSERT INTO countries(name) VALUES(‘Heard and Mc Donald Islands’);
+INSERT INTO countries(name) VALUES(‘Honduras’);
+INSERT INTO countries(name) VALUES(‘Hong Kong’);
+INSERT INTO countries(name) VALUES(‘Hungary’);
+INSERT INTO countries(name) VALUES(‘Iceland’);
+INSERT INTO countries(name) VALUES(‘India’);
+INSERT INTO countries(name) VALUES(‘Indonesia’);
+INSERT INTO countries(name) VALUES(‘Iran’);
+INSERT INTO countries(name) VALUES(‘Iraq ‘);
+INSERT INTO countries(name) VALUES(‘Ireland’);
+INSERT INTO countries(name) VALUES(‘Israel’);
+INSERT INTO countries(name) VALUES(‘Italy’);
+INSERT INTO countries(name) VALUES(‘Jamaica’);
+INSERT INTO countries(name) VALUES(‘Japan’);
+INSERT INTO countries(name) VALUES(‘Jordan’);
+INSERT INTO countries(name) VALUES(‘Kazakhstan’);
+INSERT INTO countries(name) VALUES(‘Kenya’);
+INSERT INTO countries(name) VALUES(‘Kiribati ‘);
+INSERT INTO countries(name) VALUES(‘Korea, Republic of (south)’);
+INSERT INTO countries(name) VALUES(‘Kosovo’);
+INSERT INTO countries(name) VALUES(‘Kuwait’);
+INSERT INTO countries(name) VALUES(‘Kyrgyzstan’);
+INSERT INTO countries(name) VALUES(‘Laos (Lao Peoples Democratic Republic)’);
+INSERT INTO countries(name) VALUES(‘Latvia’);
+INSERT INTO countries(name) VALUES(‘Lebanon’);
+INSERT INTO countries(name) VALUES(‘Lesotho’);
+INSERT INTO countries(name) VALUES(‘Liberia’);
+INSERT INTO countries(name) VALUES(‘Libyan Arab Jamahiriya’);
+INSERT INTO countries(name) VALUES(‘Liechtenstein’);
+INSERT INTO countries(name) VALUES(‘Lithuania’);
+INSERT INTO countries(name) VALUES(‘Luxembourg’);
+INSERT INTO countries(name) VALUES(‘Macau’);
+INSERT INTO countries(name) VALUES(‘Macedonia’);
+INSERT INTO countries(name) VALUES(‘Madagascar’);
+INSERT INTO countries(name) VALUES(‘Malawi’);
+INSERT INTO countries(name) VALUES(‘Malaysia’);
+INSERT INTO countries(name) VALUES(‘Maldives’);
+INSERT INTO countries(name) VALUES(‘Mali’);
+INSERT INTO countries(name) VALUES(‘Malta’);
+INSERT INTO countries(name) VALUES(‘Marshall Islands’);
+INSERT INTO countries(name) VALUES(‘Martinique ‘);
+INSERT INTO countries(name) VALUES(‘Mauritania’);
+INSERT INTO countries(name) VALUES(‘Mauritius’);
+INSERT INTO countries(name) VALUES(‘Mayotte’);
+INSERT INTO countries(name) VALUES(‘Mexico’);
+INSERT INTO countries(name) VALUES(‘Micronesia’);
+INSERT INTO countries(name) VALUES(‘Moldova’);
+INSERT INTO countries(name) VALUES(‘Monaco’);
+INSERT INTO countries(name) VALUES(‘Mongolia’);
+INSERT INTO countries(name) VALUES(‘Montenegro’);
+INSERT INTO countries(name) VALUES(‘Montserrat’);
+INSERT INTO countries(name) VALUES(‘Morocco’);
+INSERT INTO countries(name) VALUES(‘Mozambique’);
+INSERT INTO countries(name) VALUES(‘Myanmar’);
+INSERT INTO countries(name) VALUES(‘Namibia’);
+INSERT INTO countries(name) VALUES(‘Nauru’);
+INSERT INTO countries(name) VALUES(‘Nepal’);
+INSERT INTO countries(name) VALUES(‘Netherlands’);
+INSERT INTO countries(name) VALUES(‘Netherlands Antilles’);
+INSERT INTO countries(name) VALUES(‘New Caledonia’);
+INSERT INTO countries(name) VALUES(‘New Zealand’);
+INSERT INTO countries(name) VALUES(‘Nicaragua’);
+INSERT INTO countries(name) VALUES(‘Niger’);
+INSERT INTO countries(name) VALUES(‘Nigeria’);
+INSERT INTO countries(name) VALUES(‘Niue’);
+INSERT INTO countries(name) VALUES(‘Norfolk Island’);
+INSERT INTO countries(name) VALUES(‘Northern Mariana Islands’);
+INSERT INTO countries(name) VALUES(‘Norway’);
+INSERT INTO countries(name) VALUES(‘Oman’);
+INSERT INTO countries(name) VALUES(‘Pakistan’);
+INSERT INTO countries(name) VALUES(‘Palau’);
+INSERT INTO countries(name) VALUES(‘Palestine’);
+INSERT INTO countries(name) VALUES(‘Panama’);
+INSERT INTO countries(name) VALUES(‘Papua New Guinea’);
+INSERT INTO countries(name) VALUES(‘Paraguay ‘);
+INSERT INTO countries(name) VALUES(‘Peru’);
+INSERT INTO countries(name) VALUES(‘Philippines’);
+INSERT INTO countries(name) VALUES(‘Pitcairn’);
+INSERT INTO countries(name) VALUES(‘Poland’);
+INSERT INTO countries(name) VALUES(‘Portugal’);
+INSERT INTO countries(name) VALUES(‘Puerto Rico’);
+INSERT INTO countries(name) VALUES(‘Qatar’);
+INSERT INTO countries(name) VALUES(‘Reunion’);
+INSERT INTO countries(name) VALUES(‘Romania’);
+INSERT INTO countries(name) VALUES(‘Russian Federation’);
+INSERT INTO countries(name) VALUES(‘Rwanda’);
+INSERT INTO countries(name) VALUES(‘Saint Helena (UK)’);
+INSERT INTO countries(name) VALUES(‘Saint Kitts and Nevis’);
+INSERT INTO countries(name) VALUES(‘Saint Lucia’);
+INSERT INTO countries(name) VALUES(‘Saint Maarten’);
+INSERT INTO countries(name) VALUES(‘Saint Pierre and Miquelon (FR)’);
+INSERT INTO countries(name) VALUES(‘Saint Thomas’);
+INSERT INTO countries(name) VALUES(‘Saint Vincent and the Grenadines’);
+INSERT INTO countries(name) VALUES(‘Samoa’);
+INSERT INTO countries(name) VALUES(‘San Marino’);
+INSERT INTO countries(name) VALUES(‘Sao Tome and Principe’);
+INSERT INTO countries(name) VALUES(‘Saudi Arabia’);
+INSERT INTO countries(name) VALUES(‘Senegal’);
+INSERT INTO countries(name) VALUES(‘Serbia’);
+INSERT INTO countries(name) VALUES(‘Serbia + Montenegro (old)’);
+INSERT INTO countries(name) VALUES(‘Seychelles’);
+INSERT INTO countries(name) VALUES(‘Sierra Leone’);
+INSERT INTO countries(name) VALUES(‘Singapore’);
+INSERT INTO countries(name) VALUES(‘Slovakia’);
+INSERT INTO countries(name) VALUES(‘Slovenia’);
+INSERT INTO countries(name) VALUES(‘Solomon Islands’);
+INSERT INTO countries(name) VALUES(‘Somalia’);
+INSERT INTO countries(name) VALUES(‘South Africa’);
+INSERT INTO countries(name) VALUES(‘South Georgia and the South Sandwich Islands’);
+INSERT INTO countries(name) VALUES(‘Spain’);
+INSERT INTO countries(name) VALUES(‘Sri Lanka’);
+INSERT INTO countries(name) VALUES(‘Sudan’);
+INSERT INTO countries(name) VALUES(‘Suriname’);
+INSERT INTO countries(name) VALUES(‘Svalbard and Jan Mayen Islands’);
+INSERT INTO countries(name) VALUES(‘Swaziland’);
+INSERT INTO countries(name) VALUES(‘Sweden’);
+INSERT INTO countries(name) VALUES(‘Switzerland’);
+INSERT INTO countries(name) VALUES(‘Syrian Arab Republic’);
+INSERT INTO countries(name) VALUES(‘Taiwan’);
+INSERT INTO countries(name) VALUES(‘Tajikistan’);
+INSERT INTO countries(name) VALUES(‘Tanzania’);
+INSERT INTO countries(name) VALUES(‘Thailand’);
+INSERT INTO countries(name) VALUES(‘Timor-Leste’);
+INSERT INTO countries(name) VALUES(‘Togo’);
+INSERT INTO countries(name) VALUES(‘Tokelau’);
+INSERT INTO countries(name) VALUES(‘Tonga’);
+INSERT INTO countries(name) VALUES(‘Trinidad and Tobago’);
+INSERT INTO countries(name) VALUES(‘Tunisia’);
+INSERT INTO countries(name) VALUES(‘Turkey’);
+INSERT INTO countries(name) VALUES(‘Turkmenistan’);
+INSERT INTO countries(name) VALUES(‘Turks and Caicos Islands’);
+INSERT INTO countries(name) VALUES(‘Tuvalu’);
+INSERT INTO countries(name) VALUES(‘Uganda’);
+INSERT INTO countries(name) VALUES(‘Ukraine’);
+INSERT INTO countries(name) VALUES(‘United Arab Emirates’);
+INSERT INTO countries(name) VALUES(‘United Kingdom’);
+INSERT INTO countries(name) VALUES(‘United States of America’);
+INSERT INTO countries(name) VALUES(‘Uruguay’);
+INSERT INTO countries(name) VALUES(‘Uzbekistan’);
+INSERT INTO countries(name) VALUES(‘Vanuatu’);
+INSERT INTO countries(name) VALUES(‘Vatican City State’);
+INSERT INTO countries(name) VALUES(‘Venezuela’);
+INSERT INTO countries(name) VALUES(‘Vietnam’);
+INSERT INTO countries(name) VALUES(‘Virgin Islands (British)’);
+INSERT INTO countries(name) VALUES(‘Virgin Islands (U.S.)’);
+INSERT INTO countries(name) VALUES(‘Wallis and Futuna Islands’);
+INSERT INTO countries(name) VALUES(‘Western Sahara’);
+INSERT INTO countries(name) VALUES(‘Yemen’);
+INSERT INTO countries(name) VALUES(‘Zaire’);
+INSERT INTO countries(name) VALUES(‘Zambia’);
+INSERT INTO countries(name) VALUES(‘Zimbabwe’);
 
 -- --------------------------------------------------------
 
@@ -296,19 +293,19 @@ INSERT INTO `countries` (`country_id`, `name`) VALUES
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `location` int(11) NOT NULL,
+  `countries_id` int(11) NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  KEY `location` (`location`)
+  FOREIGN KEY (`countries_id`) REFERENCES countries(`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `location`, `email`, `password`) VALUES
+INSERT INTO `users` (`id`, `name`, `countries_id`, `email`, `password`) VALUES
 (1, 'andrea', 5, 'andrea.ialenti@gmail.com', '12345'),
 (2, 'francesco', 8, 'dsad@dsadasdjas.com', '12345'),
 (3, 'mario', 9, 'dsad@ojuieroor.com', '12345');
