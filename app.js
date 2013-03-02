bc9774e8b5d892029fa954092941471b628906b7
andrea <andrea@andrea-HP-Pavilion-g6-Notebook-PC>
2013-03-02 15:53:03 +0100

    commit dopo connessione al database (non so se sono escusi i file di netbeans)

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ var express = require('express')
 , http = require('http')
 , path = require('path');
 
+var mysql      = require('mysql');
+var controller = require("./node_modules/application/controller.js");
+require('ember');
+MyApp = Ember.Application.create({
+  hi: function() { console.log('Hi! I\'m an app!'); }
+});
+
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
-  app.engine('html', require('hbs').__express);
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -28,13 +35,14 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// dispatcher
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
 
-var mysql      = require('mysql');
-var controller = require("./node_modules/application/controller.js");
+
