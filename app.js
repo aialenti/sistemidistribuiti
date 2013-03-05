5853762a5434d9009654e430e8fc380fd22f1926
andrea <andrea@andrea-HP-Pavilion-g6-Notebook-PC>
2013-03-05 12:55:25 +0100

    commit dopo connessione al database (non so se sono escusi i file di netbeans)

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var controller = require("./node_modules/application/controller.js");
 var app = express();
 
 app.configure(function(){
-  app.set('port', 3000);
+  app.set('port', 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -35,6 +35,16 @@ app.get('/login', controller.manageMatch);
 app.get('/', controller.getIndex);
 
 
-http.createServer(app).listen(app.get('port'), function(){
+serv = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(serv);
+
+io.sockets.on('connection', function (socket) {
+  console.log("dsadas")
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
