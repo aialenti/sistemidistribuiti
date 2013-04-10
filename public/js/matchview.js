5df39034e27076986a9faba181aab7bde163095b
Francesco A <francesco.anderloni@gmail.com>
2013-04-10 15:41:47 +0200

    Completato aggiornamento risultati. Da fare: effetti visivi

--- public/js/matchview.js
+++ public/js/matchview.js
@@ -5,6 +5,7 @@ for (var i=0; i<10; i++) {
 $(document).ready(function(){
 
 	var socket = io.connect('http://localhost:3001');
+	var seasons = [];
 
 	socket.emit("getSeasons");
 	socket.on("hereSeasonsList", function (data) {
@@ -15,14 +16,21 @@ $(document).ready(function(){
 			$('#selectSeason')
 				.append($('<option>', { "value" : datas.season })
 					.text(datas.season));
+			seasons.push(datas.season);
+			console.log(seasons);
 		};
 	});
 
 	socket.emit("getLatestMatch");
 
-	$(window).load(function() { $("#selectSeason").trigger("change", 2035) });
+	//$(window).load(function() { $("#selectSeason").trigger("change", seasons[0]) });
+	window.setTimeout(function() {
+		$("#selectSeason").trigger("change", seasons[0]);
+		console.log("hhh"); 
+	}, 300);
 	
 	var matchesdata;
+	var days = [];
 
 	$("#selectSeason").change(function(e,d){
 		d = d || $(this).val();
@@ -54,7 +62,7 @@ $(document).ready(function(){
 					flag: matchesdata[i].matchdays_flag,
 				};
 				console.log("MATCH: "+datas.day+"-"+datas.flag+", PAST?"+matchesdata[i].past);
-				if (matchesdata[i].past != 1) { //h IN REALTÀ VA INVERTITO !=0
+				if (matchesdata[i].past != 0) {
 					for (var j=0; j<insertedDays.length; j++) {
 						console.log("Comparing ID " + insertedDays[j].day + "-" + insertedDays[j].flag + " with day " + datas.day + "-" + datas.flag);
 						if (insertedDays[j].day == datas.day) {
@@ -66,20 +74,25 @@ $(document).ready(function(){
 					}
 					if (!already) {
 						insertedDays.push({day: datas.day, flag: datas.flag});
+						days.push(datas.day); //jjjjj
 						console.log(insertedDays);
 						dayd = datas.day; //% (matchesdata.length/teams.length)+1;
 						console.log("DAY-- " + datas.day + " LEN-- " +matchesdata.length);
 						var leg = ((datas.flag === 0) ? "Andata" : "Ritorno");
 						$('#selectDay')
 						.append($('<option>', { "value" : datas.day+"-"+datas.flag })
-							.text(dayd + "° " + leg));
+							.text(dayd + "° Giornata - " + leg));
 					} 
 				}//h
 			};
 		});
 	});
 
-	$(window).load(function() { $("#selectDay").trigger("change", {s:2035, d:4}) });
+	//$(window).load(function() { $("#selectDay").trigger("change", {s:2035, d:4}) });
+	window.setTimeout(function() {
+		$("#selectDay").trigger("change", {s:seasons[0], d:days[0]});
+		console.log("hhh2+++ "+days[0]); 
+	}, 700);
 
 	$("#selectDay").change(function(e,p){
 		//console.log(p.d+" rrrrrr "+$(this).val() + " llll "+p.s);
@@ -129,7 +142,7 @@ $(document).ready(function(){
 				$("#accordion"+j).append('<div class="dsowag span10 offset1 well accordion-group" id="dsowag'+j+'" matchid="'+matcheslist[j].id+'"></div>');
 				$("#dsowag"+j).append('<div class="drah row accordion-heading" id="drah'+j+'"></div');
 				$("#drah"+j).append('<a class="accordion-toggle" id="toggle'+j+'" data-toggle="collapse" data-parent="#accordion'+j+'" href="#collapse'+j+'"></a>');
-				var d = createMatch(matcheslist[j]);
+				var d = createMatch(matcheslist[j],j);
 				$("#toggle"+j).append(d.d1);
 				$("#dsowag"+j).append('<div id="collapse'+j+'" class="collapsed accordion-body collapse in span10"></div>');
 				for (var k=0; k<d.d2.length; k++) {
@@ -145,15 +158,79 @@ $(document).ready(function(){
 	socket.on("updateScoresView", function(data){
 		console.log("DATA!!!!!!"+data);
 		var l = data.length;
-		console.log(data[l-1]+" "+data[l-2]);
+		console.log("L è: "+l+", data è: "+data[l-1]+" "+data[l-2]);
 		if ($("#selectSeason").val() == data[0][0].matchdays_season)
 			console.log("season ok");
 			console.log(data[l-1]+"-----v-----"+$("#selectDay").val().substring(0,1));
 			if ($("#selectDay").val().substring(0,1) == data[l-1]) {
 				console.log("$('div[matchid='"+data[l-2]+"']')==="+$('div[matchid="'+data[l-2]+'"]').html());
-				$('div[matchid="'+data[l-2]+'"]').css("background","red");
+				console.log(data[1][0].time + "oooo" + data[1][0].player);
+				var id = $('div[matchid="'+data[l-2]+'"]').attr('id').substring(6,7);
+				var len = $("#collapse"+id).children().length; //numero di righe
+				var newid = id.toString() + len;
+				var homescore = 0;
+				var awayscore = 0;
+				for (var i=1; i<data[1].length; i++) {
+					console.log("FLAG "+data[1][i].score_flag+"///"+data[1][i].id+"///"+data[l-2]);
+					if (data[1][i].id == data[l-2]) {
+						if (data[1][i].score_flag === 0)
+							homescore++;
+						else
+							awayscore++;
+					}
+				}
+				if (homescore > awayscore) {
+					if (data[1][0].score_flag == 1) {
+						var count = 0;
+						for (var j=0; j<len; j++) {
+							if ($("#row"+id+j).attr("rowcomplete") == "yes")
+								count++;
+						}
+						console.log("COUNT+"+count);
+						$("#awayscore"+id+count).text(data[1][0].time + "\' " + data[1][0].player);
+					}
+					else {
+						$("#collapse"+id).append('<div id="row'+id+len+'" class="row accordion-inner" rowcomplete="no"><div class="span1"></div><div id="homescore'+id+len+'" class="span3 offset1 score">'+data[1][0].time+'\' '+data[1][0].player+'</div><div class="span2 score"></div><div id="awayscore'+id+len+'" class="span3 offset2 score"> </div><div class="span1"></div></div>');
+					}
+				}
+				else if (awayscore > homescore) {
+					if (data[1][0].score_flag == 0) {
+						var count = 0;
+						for (var j=0; j<len; j++) {
+							if ($("#row"+id+j).attr("rowcomplete") == "yes")
+								count++;
+						}
+						console.log("COUNT+"+count);
+						$("#homescore"+id+count).text(data[1][0].time + "\' " + data[1][0].player);
+					}
+					else {
+						$("#collapse"+id).append('<div id="row'+id+len+'" class="row accordion-inner" rowcomplete="no"><div class="span1"></div><div id="homescore'+id+len+'" class="span3 offset1 score"> </div><div class="span2 score"></div><div id="awayscore'+id+len+'" class="span3 offset2 score">'+data[1][0].time+'\' '+data[1][0].player+'</div><div class="span1"></div></div>');
+					}
+				}
+				else {
+					if (data[1][0].score_flag === 0) {
+						$("#collapse"+id).append('<div id="row'+newid+'" class="row accordion-inner" rowcomplete="no"><div class="span1"></div><div id="homescore'+newid+'" class="span3 offset1 score">'+data[1][0].time+'\' '+data[1][0].player+'</div><div class="span2 score"></div><div id="awayscore'+newid+'" class="span3 offset2 score"> </div><div class="span1"></div></div>');
+					}
+					else {
+						$("#collapse"+id).append('<div id="row'+newid+'" class="row accordion-inner" rowcomplete="no"><div class="span1"></div><div id="homescore'+newid+'" class="span3 offset1 score"> </div><div class="span2 score"></div><div id="awayscore'+newid+'" class="span3 offset2 score">'+data[1][0].time+'\' '+data[1][0].player+'</div><div class="span1"></div></div>');
+					}
+
+				}
+				if (data[1][0].score_flag == 0)
+					homescore++;
+				else
+					awayscore++;
+				$('p[id="match'+id+'"]').text(homescore+" - "+awayscore);
 			}
 	});
+
+	//NOTA IMPORTANTE:
+	//Questa implementazione prevede che i risultati siano aggiornati UNO ALLA VOLTA e IN ORDINE TEMPORALE 
+	//(ovvero "seguendo le partite", ogni volta che c'è un gol si aggiorna, supponendo che tutte le partite della
+	//stessa giornata comincino e finiscano alla stessa ora! Se no è un casino!)Questo perché controller.js
+	//restituisce le score ordinate per "time" (minuto del gol)
+	//Se si vuole mettere risultati di giornate vecchie invece non è un problema, tanto la view
+	//viene costrutita correttamente ad ogni richiesta
 
 	$("#loginform").submit(function() {
 
@@ -185,7 +262,7 @@ $(document).ready(function(){
 	});
 });
 
-var createMatch = function(match) {
+var createMatch = function(match,number) {
 	var homeGoals = [];
 	var awayGoals = [];
 	for (var l=0; l<match.goals.length; l++) {
@@ -197,17 +274,17 @@ var createMatch = function(match) {
 	homeGoals.sort(function(a,b) {return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0);} );
 	awayGoals.sort(function(a,b) {return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0);} );
 	divs = new Object();
-	divs.d1 = '<div class="span1 scudo"> <img src="/'+match.home_team+'.png" alt="" /></div><p class="span3 team">'+match.home_team_name+'</p><p class="span2 team">'+homeGoals.length+' - '+awayGoals.length+'</p><p class="span3 team">'+match.away_team_name+'</p><div class="span1 scudo"> <img src="/'+match.away_team+'.png" alt="" /></div>'
+	divs.d1 = '<div class="span1 scudo"> <img src="/'+match.home_team+'.png" alt="" /></div><p class="span3 team">'+match.home_team_name+'</p><p id="match'+number+'" class="span2 team">'+homeGoals.length+' - '+awayGoals.length+'</p><p class="span3 team">'+match.away_team_name+'</p><div class="span1 scudo"> <img src="/'+match.away_team+'.png" alt="" /></div>'
 	var d2 = [];
 	if (homeGoals.length > awayGoals.length) {
 		var diff = homeGoals.length - awayGoals.length;
 		var o = 0;
 		for (o; o<awayGoals.length; o++) {
-			var dd = '<div class="row accordion-inner"><div class="span1"></div><div class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
+			var dd = '<div class="row accordion-inner" id="row'+number+o+'" rowcomplete="yes"><div class="span1"></div><div id="homescore'+number+o+'" class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div id="awayscore'+number+o+'" class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
 			d2.push(dd);
 		}
 		for (o; o<homeGoals.length; o++) {
-			var dd = '<div class="row accordion-inner"><div class="span1"></div><div class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div class="span3 offset2 score"> </div><div class="span1"></div></div>';
+			var dd = '<div class="row accordion-inner" id="row'+number+o+'" rowcomplete="no"><div class="span1"></div><div id="homescore'+number+o+'" class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div id="awayscore'+number+o+'" class="span3 offset2 score"> </div><div class="span1"></div></div>';
 			d2.push(dd);
 		}
 	}
@@ -215,17 +292,17 @@ var createMatch = function(match) {
 		var diff = awayGoals.length - homeGoals.length;
 		var o = 0;
 		for (o; o<homeGoals.length; o++) {
-			var dd = '<div class="row accordion-inner"><div class="span1"></div><div class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
+			var dd = '<div class="row accordion-inner" id="row'+number+o+'" rowcomplete="yes"><div class="span1"></div><div id="homescore'+number+o+'" class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div id="awayscore'+number+o+'" class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
 			d2.push(dd);
 		}
 		for (o; o<awayGoals.length; o++) {
-			var dd = '<div class="row accordion-inner"><div class="span1"></div><div class="span3 offset1 score">  </div><div class="span2 score"></div><div class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
+			var dd = '<div class="row accordion-inner" id="row'+number+o+'" rowcomplete="no"><div class="span1"></div><div id="homescore'+number+o+'" class="span3 offset1 score">  </div><div class="span2 score"></div><div id="awayscore'+number+o+'" class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
 			d2.push(dd);
 		}
 	}
 	else {
 		for (var o=0; o<homeGoals.length; o++) {
-			var dd = '<div class="row accordion-inner"><div class="span1"></div><div class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
+			var dd = '<div class="row accordion-inner" id="row'+number+o+'" rowcomplete="yes"><div class="span1"></div><div id="homescore'+number+o+'" class="span3 offset1 score">'+homeGoals[o].time+'\' '+homeGoals[o].player+'</div><div class="span2 score"></div><div id="awayscore'+number+o+'" class="span3 offset2 score">'+awayGoals[o].time+'\' '+awayGoals[o].player+'</div><div class="span1"></div></div>';
 			d2.push(dd);
 		}
 	}
